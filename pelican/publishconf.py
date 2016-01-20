0438a5089589726ebdbfe1e92321d53456568c0b
40423227 <jackson860711@gmail.com>
2016-01-20 21:25:27 +0800

    4042323636

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -30,7 +30,7 @@ SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423236 github','http://40423236.github.io/CPB_40423236/'))
+         ('Python', 'http://python.org/'),('2015作業主頁', '../../'),('40423236 個人網頁','http://40423236.github.io/CPB_40423236/'))
 
 
 
